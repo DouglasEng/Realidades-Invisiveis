69a1317b061b406923b4305838f90bd7e3e2f693
Douglas <douglasrodriguesgithub@gmail.com>
2025-06-23 14:46:59 -0300

    [ 🎨 ] Animação de entrada

--- js/game-animations.js
+++ js/game-animations.js
@@ -1,12 +1,23 @@
-    document.addEventListener("DOMContentLoaded", () => {
-      const frase1 = document.getElementById("frase1");
-      const frase2 = document.getElementById("frase2");
-      const finalIntro = document.getElementById("final-intro");
-      const botao = document.getElementById("botao-start");
+document.addEventListener("DOMContentLoaded", () => {
+  const telaTerror = document.getElementById("tela-terror");
+  const telaIntrodutoria = document.getElementById("tela-introdutoria");
+  const frase1 = document.getElementById("frase1");
+  const frase2 = document.getElementById("frase2");
+  const finalIntro = document.getElementById("final-intro");
+  const botao = document.getElementById("botao-start");
 
+  setTimeout(() => {
+    telaTerror.classList.add("sair");
+    
+    // depois de 2 segundos de trasição aparece a tela introdutoria
+    setTimeout(() => {
+      telaTerror.classList.remove("ativa");
+      telaIntrodutoria.classList.add("ativa");
+      
+      // animação de titulo
       frase1.classList.add("animar-frase-1");
       frase2.classList.add("animar-frase-2");
-
+      
       setTimeout(() => {
         finalIntro.classList.add("aparecer");
       }, 10600);
@@ -14,4 +25,7 @@
       setTimeout(() => {
         botao.classList.add("entrar");
       }, 11800);
-    });
+      
+    }, 2000);
+  }, 8000);
+});
